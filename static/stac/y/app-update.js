7d29896a250357dd9943999a121047a36e98cd6d
Sai Panneerselvam <89541946+SaiPanneerselvam@users.noreply.github.com>
2023-11-17 19:40:06 -0500

    cansel

--- static/stac/y/app-update.js
+++ static/stac/y/app-update.js
@@ -1,1 +1,10 @@
+var txt = document.getElementById("appsmenu"); // origin hw
+var txs = document.getElementById("sha"); // origin sha
 
+function fod() {
+  // Get the content of the element with id "hw" and concatenate it with the iframe source
+  var sd = '<p>Hi</p>';
+  
+  // Insert the iframe into the element with id "sha"
+  txs.innerHTML = sd;
+}
